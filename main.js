9f1b2004c9970af11ff43a26e7aac650921dff25
Justin Hellen <jmhellen1@gmail.com>
2019-10-14 13:07:06 -0400

    added js functionality to nav menu

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 function init() {
   $('nav a').click(e => {
-    // e.preventDefault();
+    e.preventDefault();
     // console.log(this);
     let anchor = $(e.target).attr('data-link');
     // let anchor = $(this).data('link');
-    scrollToAnchor(anchor);
-  })  
+    scrollToAnchor(anchor, 100);
+  })
+  $('.linkDrop a').click(e => {
+    e.preventDefault();
+    // console.log(this);
+    let anchor = $(e.target).attr('data-link');
+    // let anchor = $(this).data('link');
+    scrollToAnchor(anchor, 140);
+  })
+  $('.hamburger').click(e => {
+    e.preventDefault();
+    console.log('hamburger clicked');
+    $('.linkDrop').toggleClass('showMenu')
+  })
 }
-function scrollToAnchor(scrollTarget){
+function scrollToAnchor(scrollTarget, offset){
     console.log('trying to scroll to', scrollTarget)
     let aTag = $(`section[class='${scrollTarget}']`);
-    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+    let scrollDest = aTag.offset().top - offset;
+    $('html,body').animate({scrollTop: scrollDest},'slow');
 }
 $(init)
+
